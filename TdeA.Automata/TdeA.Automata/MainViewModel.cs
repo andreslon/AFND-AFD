7721fc447624b747f888251b88519332b33b7e9d
Andrés Londoño <andreslon@outlook.com>
2018-05-28 00:29:42 -0500

    Proyecto base automatas

---
+++ TdeA.Automata/TdeA.Automata/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TdeA.Automata
+{
+    public class MainViewModel
+    { 
+        private List<Country> _Values;
+
+        public List<Country> Values
+        {
+            get { return _Values; }
+            set { _Values = value; }
+        }
+        public MainViewModel()
+        {
+            this.Values = new List<Country>();
+            this.Values.Add(new Country { Name = "America" });
+            this.Values.Add(new Country { Name = "Africa" });
+            this.Values.Add(new Country { Name = "India" });
+        }
+    }
+    public class Country
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ TdeA.Automata/TdeA.Automata/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TdeA.Automata
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+        private static bool IsTextAllowed(string text)
+        {
+            Regex regex = new Regex("[^0-9]+"); //regex that matches disallowed text
+            return !regex.IsMatch(text);
+        }
+    }
+}
